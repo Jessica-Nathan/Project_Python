e710b758c9aef4070af9bc5680ac84b8399278c4
Jessica-Nathan <Jessica.Nathan@dal.ca>
2018-05-18 13:09:36 -0300

    second

--- part1.py
+++ part1.py
@@ -4,8 +4,18 @@ apikey='Basic ODQxZTJhMjA2Yjc1NDczY2ExMTM0ZTA5ZjMxYmE3Nzc6YmZlMGQ1ZjA4ZjY3NDdlZD
 header={'Authorization' :apikey,'Content-Type':'application/x-www-form-urlencoded'}
 oid=requests.get("https://management.api.umbrella.com/v1/organizations/",headers=header)
 oid=oid.json()
-orgid=oid[0]["organizationId"]
+orgid=str(oid[0]["organizationId"])
 print("The organisation Id is {}" .format(orgid))
+
+
+def List_Network_devices():
+	print (header)
+	list_devices=requests.get("https://management.api.umbrella.com/v1/organizations/" + orgid +"/networkdevices/ ",headers=header)
+	list_devices=list_devices.json()
+	return list_devices
+
+print(List_Network_devices())
+	
 
 
 
